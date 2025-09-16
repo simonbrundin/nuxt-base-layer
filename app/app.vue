dfd94caf798909242c5fe340d4a3909ebe1950ad
Simon Brundin <simonbrundin@gmail.com>
2025-09-16 22:08:19 +0200

    updates

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  compatibilityDate: "2025-07-15",
+  modules: ["shadcn-nuxt", "@nuxtjs/tailwindcss", "@nuxt/icon"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./app/components/ui",
+  },
+});
